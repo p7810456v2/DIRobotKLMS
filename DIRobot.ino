efc8f0d735e82dcb5d0455b54ac18936f7f8eb7c
PranitA <carboy359@gmail.com>
2018-02-21 23:10:50 -0600

    Updates to the source code to make it to work

--- DIRobot.ino
+++ DIRobot.ino
@@ -1,12 +1,15 @@
 #include <AFMotor.h> //Library for motors
 #include <SPI.h> //Default SPI library
 #include <RF24.h> //Library for the radio
-
-#define powerLevel RF24_PA_LOW /*Change this to change
+#include <Servo.h>
+#define powerLevel RF24_PA_MIN /*Change this to change
 the radio power level*/
-
-#define transPin A8
-
+#define rfReceivePin A0  //RF Receiver pin = Analog pin 0
+#define transPin A8 
+unsigned int dat = 0;   // variable used to store received data
+const int upperThreshold = 70;  //upper threshold value
+const int lowerThreshold = 50;  //lower threshold value
+int angle = 0;  
 AF_DCMotor motor1(1); //Set up motors
 AF_DCMotor motor2(2); //Set up motors
 AF_DCMotor motor3(3); //Set up motors
@@ -20,12 +23,13 @@ unsigned long prevMills = 0;
 const long interval = 10;
 byte recievedPayload; /* The payload recieved from
 the controller*/
-
+ 
 /*byte address[6]; Address for the radio
 pipe, 40-bit, so 10 hexadecimal numbers, array should be
 5 bytes long, but set to 6 just in case*/
 
 void setup() {
+  //servo_test.attach(9); 
   pinMode(transPin, OUTPUT);
   pinMode(13, OUTPUT);
   radio.begin();
@@ -37,25 +41,27 @@ void setup() {
   motor2.setSpeed(200); //Set the motor speeds
   motor3.setSpeed(200); //Set the motor speeds
   motor4.setSpeed(200); //Set the motor speeds
+
+  radio.setAutoAck(true);
 }
 
 void loop() {
-  unsigned long currMillis = millis();
-  if(radio.available()){
+  //unsigned long currMillis = millis();
+  //if(radio.available()){
     radio.read(&recievedPayload, sizeof(byte));
     digitalWrite(13, HIGH);
-    }
-  else{
-    digitalWrite(13, LOW);
-  }
-  if(currMillis - prevMillis > interval){
+    //}
+  //else{
+  //  digitalWrite(13, LOW);
+  //}
+  /*if(currMillis - prevMillis > interval){
     prevMillis = currMillis;
   }
   if(transState == LOW){
     transState = HIGH;
   } else {
     transState = LOW;
-  }
+  }*/
   
   switch(recievedPayload){ //Switch payload into motors
     case 0x01:
@@ -72,7 +78,7 @@ void loop() {
       motor4.run(FORWARD);
     break;
 
-    case 0x03:
+    case 0x05:
       motor1.run(FORWARD);
       motor2.run(FORWARD);
       motor3.run(BACKWARD);
@@ -86,14 +92,28 @@ void loop() {
       motor4.run(FORWARD);
     break;
 
-    case 0x05:
+    case 0x03:
       motor1.run(BACKWARD);
       motor2.run(BACKWARD);
       motor3.run(BACKWARD);
       motor4.run(BACKWARD);
     break;
-    
-  }
+     
+      
+    }
+  dat =  analogRead(rfReceivePin);    //listen for data on Analog pin 0
+   
+    //if(dat>upperThreshold){
+     Serial.println(dat);
+     //for(angle = 0; angle < 180; angle += 1){   // command to move from 0 degrees to 180 degrees 
+    //servo_test.write(angle);                 //command to rotate the servo to the specified angle
+    //delay(15);                       
+      //}
+    //}
+   
+   if(dat<lowerThreshold){   //If a HIGH signal is received, turn LED ON
+     Serial.println(dat);
+   }
 }
 /*  Unnecessary for now...
 void motor(byte motorNumber, byte motorSpeed){
--- DIRobotTransmitter.ino
+++ DIRobotTransmitter.ino
@@ -1,64 +1,97 @@
 #include <SPI.h>
 #include <RF24.h>
 
-#define powerLevel RF24_PA_HIGH //Change this to change
+#define powerLevel RF24_PA_MIN //Change this to change
 //the power level
+#define rfTransmitPin 4 
+const int upperThreshold = 70;  //upper threshold value
+const int lowerThreshold = 50;  //lower threshold value
 
- byte button;
-
+byte button;
+int rfReceivePin;
+int joyX;
+int joyY;
+int push = 0;
+//unsigned int dat = analogRead(rfReceivePin);
 RF24 radio(7,8); //CE, CSN
 
 void setup() {
+  Serial.begin(9600);
   radio.begin();
   radio.setPALevel(powerLevel);
   radio.openWritingPipe(0x0000000001);
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
-  pinMode(A2, INPUT);
-  pinMode(A3, INPUT);
-  pinMode(6, OUTPUT);
+  //pinMode(A2, INPUT);
+  //pinMode(A3, INPUT);
+  //pinMode(6, OUTPUT);
   radio.stopListening();
+  pinMode(rfTransmitPin, OUTPUT); 
 }
 
 void loop() {
-  //Turn the button presses into hexadecimals to be sent
-  if(digitalRead(A0) == HIGH){
+  joyX = analogRead(A0);
+  joyY = analogRead(A1);
+  push = digitalRead(A2);
+  //Turn the joystick moves into hex vals to be sent
+  if(joyY <= 250){
     button = 0x02;
   }
   else{
-    if(digitalRead(A1) == HIGH){
-    button = 0x03;
+    if(joyY >= 750){
+    button = 0x05;
     }
     else{
-      if(digitalRead(A2) == HIGH){
-        button = 0x04;
+      if(joyX <= 250){
+        button = 0x03;
       }
       else{
-          if(digitalRead(A3) == HIGH){
-            button = 0x05;
+          if(joyX >= 750){
+            button = 0x04;
           }
-          else{
+          /*else{
             if(digitalRead(A4) == HIGH){
               button = 0x06;
-          }
+          }*/
             else{
               button = 0x01;
           }
+           /*if(push == LOW){
+             if(dat>upperThreshold){
+                Serial.println(dat);
+             }
+             if(dat<lowerThreshold){   //If a HIGH signal is received, turn LED ON
+                Serial.println(dat);
+            }*/
+          }
+          
       }
     }
     
    
+  //}
+  /*for(int i=4000; i>5; i=i-(i/3)){
+     digitalWrite(rfTransmitPin, HIGH);
+     digitalWrite(rfTransmitPin,LOW);
   }
+  
   /*if(digitalRead(A0) == LOW && digitalRead(A1) == LOW
   && digitalRead(A2) == LOW && digitalRead(A3) == LOW){
     button == 0x01;
   }*/
+
+  //Output joystick info
+  Serial.print("A0: ");
+  Serial.print(analogRead(A0));
+  Serial.print(" A1: ");
+  Serial.println(analogRead(A1));
   
   if(!radio.write(&button, sizeof(byte))){
     digitalWrite(6, HIGH);
+    Serial.print("It FAILED");
   }
   else{
     digitalWrite(6, LOW);
   }
 }
-}
+
